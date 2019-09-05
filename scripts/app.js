1bbe07a582d4919b5cd8a71f7900cada2ea7c821
rahulmistry33 <rahul.mistry@somaiya.edu>
2019-09-05 14:57:04 +0530

    UI logic

---
+++ scripts/app.js
@@ -0,0 +1,58 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    //update details
+    details.innerHTML = 
+    `<h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}
+      <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+      </div>
+    </div>`
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+
+};
+
+
+
+
+
+const updateCity = async (city) => {
+   
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return { cityDets,weather };
+
+};
+
+
+
+
+
+
+
+cityForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+    // get the city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset() //clearing the textbox after giving input
+
+    //update UI
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+
+
+});
